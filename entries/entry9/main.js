3ec3d5d2cd92fa5d1feec3c67a95307e01be67df
hiyasmh <singh028@newschool.edu>
2022-11-15 16:02:21 -0500

    entry9_1

---
+++ entries/entry9/main.js
@@ -0,0 +1,130 @@
+let btnA = document.querySelector('#A')
+let btnB = document.querySelector("#B")
+let btnC = document.querySelector("#C")
+let btnD = document.querySelector("#D")
+let btnE = document.querySelector("#E")
+let btnF = document.querySelector("#F")
+let btnG = document.querySelector("#G")
+let btnH = document.querySelector("#H")
+let btnI = document.querySelector("#I")
+let btnJ = document.querySelector("#J")
+let btnK = document.querySelector("#K")
+let btnL = document.querySelector("#L")
+let btnM = document.querySelector("#M")
+let btnN = document.querySelector("#N")
+let btnO = document.querySelector("#O")
+let btnP = document.querySelector("#P")
+let btnQ = document.querySelector("#Q")
+let btnR = document.querySelector("#R")
+let btnS = document.querySelector("#S")
+let btnT = document.querySelector("#T")
+let btnU = document.querySelector("#U")
+let btnV = document.querySelector("#V")
+let btnW = document.querySelector("#W")
+let btnX = document.querySelector("#X")
+let btnY = document.querySelector("#Y")
+let btnZ = document.querySelector("#Z")
+
+btnA.addEventListener('click', function onClick() {
+  btnA.style.backgroundColor = 'teal';
+});
+
+btnB.addEventListener('click', function onClick() {
+  btnB.style.backgroundColor = 'gray';
+});
+
+btnC.addEventListener('click', function onClick() {
+  btnC.style.backgroundColor = 'yellow';
+});
+
+btnD.addEventListener('click', function onClick() {
+  btnD.style.backgroundColor = 'seagreen';
+});
+
+btnE.addEventListener('click', function onClick() {
+  btnE.style.backgroundColor = 'orange';
+});
+
+btnF.addEventListener('click', function onClick() {
+  btnF.style.backgroundColor = 'lightgoldenrodyellow';
+});
+
+btnG.addEventListener('click', function onClick() {
+  btnG.style.backgroundColor = 'indigo';
+});
+
+btnH.addEventListener('click', function onClick() {
+  btnH.style.backgroundColor = 'darkred';
+});
+
+btnI.addEventListener('click', function onClick() {
+  btnI.style.backgroundColor = 'green';
+});
+
+btnJ.addEventListener('click', function onClick() {
+  btnJ.style.backgroundColor = 'red';
+});
+
+btnK.addEventListener('click', function onClick() {
+  btnK.style.backgroundColor = 'coral';
+});
+
+btnL.addEventListener('click', function onClick() {
+  btnL.style.backgroundColor = 'indianred';
+});
+
+btnM.addEventListener('click', function onClick() {
+  btnM.style.backgroundColor = 'magenta';
+});
+
+btnN.addEventListener('click', function onClick() {
+  btnN.style.backgroundColor = 'skyblue';
+});
+
+btnO.addEventListener('click', function onClick() {
+  btnO.style.backgroundColor = 'cyan';
+});
+
+btnP.addEventListener('click', function onClick() {
+  btnP.style.backgroundColor = 'fuchsia';
+});
+
+btnQ.addEventListener('click', function onClick() {
+  btnQ.style.backgroundColor = 'floralwhite';
+});
+
+btnR.addEventListener('click', function onClick() {
+  btnR.style.backgroundColor = 'aquamarine';
+});
+
+btnS.addEventListener('click', function onClick() {
+  btnS.style.backgroundColor = 'burlywood';
+});
+
+btnT.addEventListener('click', function onClick() {
+  btnT.style.backgroundColor = 'olive';
+});
+
+btnU.addEventListener('click', function onClick() {
+  btnU.style.backgroundColor = 'brown';
+});
+
+btnV.addEventListener('click', function onClick() {
+  btnV.style.backgroundColor = 'ivory';
+});
+
+btnW.addEventListener('click', function onClick() {
+  btnW.style.backgroundColor = 'greenyellow';
+});
+
+btnX.addEventListener('click', function onClick() {
+  btnX.style.backgroundColor = 'peachpuff';
+});
+
+btnY.addEventListener('click', function onClick() {
+  btnY.style.backgroundColor = 'blueviolet';
+});
+
+btnZ.addEventListener('click', function onClick() {
+  btnZ.style.backgroundColor = 'paleturquoise';
+});
