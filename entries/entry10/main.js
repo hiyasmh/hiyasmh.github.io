4705cba2655b4c4054bb17f37db7c5b4b7a17e88
hiyasmh <singh028@newschool.edu>
2022-11-22 17:08:24 -0500

    entry10_p1

---
+++ entries/entry10/main.js
@@ -0,0 +1,12 @@
+console.log("hello")
+
+let keys = document.querySelectorAll('.key')
+
+keys.forEach(key =>{
+	key.addEventListener('click', () => playNote(key))
+})
+
+function playNote(key) {
+	let noteAudio = document.getElementById(key.dataset.note)
+	noteAudio.play()
+}
