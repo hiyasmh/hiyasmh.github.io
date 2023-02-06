55f74d7bafebb393b425351fdded969a418bb6d5
hiyasmh <singh028@newschool.edu>
2023-02-06 05:29:21 -0500

    storytime

---
+++ cis23/Storytime/dex2/main.js
@@ -0,0 +1,12 @@
+console.log("hey Nika!");
+
+let btns = document.querySelectorAll('.btn')
+
+btns.forEach(btn =>{
+	btn.addEventListener('click', () => playNote(btn))
+})
+
+function playNote(btn) {
+	let noteAudio = document.getElementById(btn.dataset.note)
+	noteAudio.play()
+}
---
+++ cis23/Storytime/dex3/main.js
@@ -0,0 +1,76 @@
+console.log("Hey Nika!")
+
+function fightNormal() {
+	document.body.style.backgroundImage ="url(imgs/ghost.png)";
+}
+
+function fightFire() {
+	document.body.style.backgroundImage ="url(imgs/water.png)";
+}
+
+function fightWater() {
+	document.body.style.backgroundImage ="url(imgs/electric.png)";
+}
+
+function fightElectric() {
+	document.body.style.backgroundImage ="url(imgs/ground.png)";
+}
+
+function fightGrass() {
+	document.body.style.backgroundImage ="url(imgs/bug.png)";
+}
+
+function fightIce() {
+	document.body.style.backgroundImage ="url(imgs/steel.png)";
+}
+
+function fightFighting() {
+	document.body.style.backgroundImage ="url(imgs/flying.png)";
+}
+
+function fightPoison() {
+	document.body.style.backgroundImage ="url(imgs/psychic.png)";
+}
+
+function fightGround() {
+	document.body.style.backgroundImage ="url(imgs/ice.png)";
+}
+
+function fightFlying() {
+	document.body.style.backgroundImage ="url(imgs/rock.png)";
+}
+
+function fightPsychic() {
+	document.body.style.backgroundImage ="url(imgs/dark.png)";
+}
+
+function fightBug() {
+	document.body.style.backgroundImage ="url(imgs/fire.png)";
+}
+
+function fightRock() {
+	document.body.style.backgroundImage ="url(imgs/grass.png)";
+}
+
+function fightGhost() {
+	document.body.style.backgroundImage ="url(imgs/normal.png)";
+}
+
+function fightDragon() {
+	document.body.style.backgroundImage ="url(imgs/dragon.png)";
+}
+
+function fightDark() {
+	document.body.style.backgroundImage ="url(imgs/fairy.png)";
+}
+
+function fightSteel() {
+	document.body.style.backgroundImage ="url(imgs/fighting.png)";
+}
+
+function fightFairy() {
+	document.body.style.backgroundImage ="url(imgs/poison.png)";
+}
+
+
+
