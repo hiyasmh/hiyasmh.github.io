5fce0b2c4f20e454b681ea7dacc43053dcc90cf1
hiyasmh <singh028@newschool.edu>
2023-02-19 17:36:04 -0500

    project1_edit1

--- cis23/project1/home/main.js
+++ cis23/project1/home/main.js
@@ -1,12 +1,23 @@
 console.log("Hey Nika!");
 
 let btns = document.querySelectorAll('.btn');
+let cntrls = document.querySelectorAll('.controls')
 
 btns.forEach(btn =>{
 	btn.addEventListener('click', () => playNote(btn));
+})
+
+cntrls.forEach(btn =>{
+	cntrls.addEventListener('click', () => pauseNote(cntrls));
 })
 
 function playNote(btn) {
 	let noteAudio = document.getElementById(btn.dataset.note);
 	noteAudio.play()
 }
+
+function pauseNote(controls) {
+	let pauseAudio = document.getElementById(controls.dataset.note);
+	pauseAudio.pause()
+}
+
