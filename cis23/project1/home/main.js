08742a10f30d13a8fb6a2b6e13ac03d53ce36133
hiyasmh <singh028@newschool.edu>
2023-02-19 22:09:22 -0500

    edit3_pro1

--- cis23/project1/home/main.js
+++ cis23/project1/home/main.js
@@ -22,3 +22,36 @@ document.getElementById('plus').addEventListener('click', () => {
 document.getElementById('minus').addEventListener('click', () => {
   document.querySelectorAll('audio').forEach(el => el.currentTime -= 1.0)
 });
+
+
+function qmCursor(){
+	document.getElementById("body").style.cursor = "url(assets/qm.png), auto";
+}
+
+function colonCursor(){
+	document.getElementById("body").style.cursor = "url(assets/colon.png), auto";
+}
+
+function semicolonCursor(){
+	document.getElementById("body").style.cursor = "url(assets/semicolon.png), auto";
+}
+
+function apsCursor(){
+	document.getElementById("body").style.cursor = "url(assets/aps.png), auto";
+}
+
+function qtsCursor(){
+	document.getElementById("body").style.cursor = "url(assets/qts.png), auto";
+}
+
+function commaCursor(){
+	document.getElementById("body").style.cursor = "url(assets/comma.png), auto";
+}
+
+function periodCursor(){
+	document.getElementById("body").style.cursor = "url(assets/period.png), auto";
+}
+
+function exclamationCursor(){
+	document.getElementById("body").style.cursor = "url(assets/exclamation.png), auto";
+}
